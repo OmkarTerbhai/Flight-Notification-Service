7643ad5ab20294e22fff9c87e2b62d14ddde7570
OmkarTerbhai <omkarterbhai@gmail.com>
2023-06-04 12:52:03 +0530

    Created ticket model

---
+++ src/migrations/20230604070959-create-ticket.js
@@ -0,0 +1,41 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      subject: {
+        type: Sequelize.STRING
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      recipientEmail: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM,
+        values: ['Pending', 'Success', 'Failed'],
+      allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tickets');
+  }
+};
